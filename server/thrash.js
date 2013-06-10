8700773c4ce880d2f37ff37c455bb12c9d9dc254
Ben Wiklund <ben@daisyowl.com>
2013-06-09 20:40:44 -0700

    updated thrasher

--- server/thrash.js
+++ server/thrash.js
@@ -1,6 +1,10 @@
-while(true){
+var thrash = function(){
   var uselessData = [];
-  for( var i = 0; i < 100000000; i++ ){
+  for( var i = 0; i < 10000000; i++ ){
     uselessData.push( Math.random() );
   }
+  setTimeout(thrash,1000);
 }
+
+thrash()
+
